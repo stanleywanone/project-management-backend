a4b1db0224aeb153934016324d98821ee0c3d7b4
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-08-17 21:38:07 -0500

    create porject_type in the model

--- projectsApp/models.py
+++ projectsApp/models.py
@@ -4,11 +4,12 @@ import uuid
 # Create your models here.
 class Projects(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    project_title = models.CharField(max_length=50)
+    project_title = models.CharField(max_length=225)
     project_description = models.CharField(max_length=225)
-    project_progress = models.CharField(max_length=50)
-    project_priority = models.CharField(max_length=50, null=True)
-    created_by = models.CharField(max_length=50, null=True)
+    project_progress = models.CharField(max_length=225)
+    project_priority = models.CharField(max_length=225, null=True)
+    project_type = models.CharField(max_length=225, null=True)
+    created_by = models.CharField(max_length=225, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
    
--- projectsApp/serializers.py
+++ projectsApp/serializers.py
@@ -4,4 +4,4 @@ from projectsApp.models import Projects
 class ProjectsSerializer(serializers.ModelSerializer):
     class Meta:
         model=Projects
-        fields=('id', 'project_title', 'project_description', 'project_progress', 'created_by', 'created_at','updated_at', 'project_priority')
+        fields=('id', 'project_title', 'project_description', 'project_progress', 'project_type', 'created_by', 'created_at','updated_at', 'project_priority')
