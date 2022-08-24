baa08ef1f645815c0198d1583b6a0a1f8d543c24
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-08-23 21:15:46 -0500

    use uuid in the frontend instead of backend

--- projectsApp/models.py
+++ projectsApp/models.py
@@ -3,7 +3,7 @@ import uuid
 
 # Create your models here.
 class Projects(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True,  max_length=225)
     project_title = models.CharField(max_length=225)
     project_description = models.CharField(max_length=225)
     project_progress = models.CharField(max_length=225)
